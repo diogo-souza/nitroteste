8ce285170ffa1532cf3e71223ae7a3b56fe7ce81
diogo-souza <digsuk360@gmail.com>
2023-08-19 23:28:36 -0300

    automatizado with Grunt

---
+++ dist/js/App.js
@@ -0,0 +1,8 @@
+import './App.scss';
+import SearchPage from './pages/SearchPage';
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement(SearchPage, null));
+}
+export default App;
